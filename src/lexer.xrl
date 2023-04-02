4aecbead908a1d343377a413e5e9f30908cae588
OshkarVTec <a01275287@tec.mx>
2023-04-02 16:09:41 -0600

    Hexadecimal integer

--- src/lexer.xrl
+++ src/lexer.xrl
@@ -1,7 +1,7 @@
 Definitions.
 Rules.
-[0-9](_?[0-9])* :  {token, {integer, TokenLine, TokenChars}}. 
-0[xX](_?[0-9a-fA-F])+   : {token, {integer, TokenLine, TokenChars}}.
+[0-9](_?[0-9])* :  {token, {integer, TokenLine, TokenChars}}. %Decimal integer
+0[xX](_?[0-9a-fA-F])+   : {token, {integer, TokenLine, TokenChars}}. %Hex integer
 \s             : skip_token.
 Erlang code.
 
--- test/python_lexer_test.exs
+++ test/python_lexer_test.exs
@@ -2,11 +2,20 @@ defmodule PythonLexerTest do
   use ExUnit.Case
   doctest PythonLexer
 
-  test "decinteger1" do
+  test "decInteger1" do
     assert {:ok, [{:integer, _,_}],_} = :lexer.string('422')
   end
-  test "decinteger2" do
+  test "decInteger2" do
     assert {:ok, [{:integer, _,_}],_} = :lexer.string('100_000')
+  end
+  test "hexInteger1" do
+    assert {:ok, [{:integer, _,_}],_} = :lexer.string('0x2A')
+  end
+  test "hexInteger2" do
+    assert {:ok, [{:integer, _,_}],_} = :lexer.string('0x23_9F')
+  end
+  test "hexInteger3" do
+    assert {:ok, [{:integer, _,_}],_} = :lexer.string('0X4AB')
   end
 
 end
